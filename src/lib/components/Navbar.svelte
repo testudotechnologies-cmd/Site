a6527a4238bf0e8e3d160cd89b0e4d96fee9e751
Filipe Bertolucci <filipe@bertolucci.it>
2026-03-06 20:36:17 +0100

    navbar + theme colors + logo

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,50 @@
+<script>
+  export let logo = "../assets/logo-inverted.png";
+  export let brand = "MyApp";
+</script>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+
+    <!-- Logo / Brand -->
+    <a class="navbar-brand d-flex align-items-center" href="/">
+      <img src={logo} alt="logo" height="40" class="me-2" />
+      {brand}
+    </a>
+
+    <!-- Mobile Toggle -->
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar Links -->
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+
+        <li class="nav-item">
+          <a class="nav-link active" href="/">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/about">About</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/services">Services</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+
+      </ul>
+    </div>
+
+  </div>
+</nav>
