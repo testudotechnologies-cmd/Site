a45f39ee679317dda8c758adac46da908a684fc4
Filipe Bertolucci <filipe@bertolucci.it>
2026-03-09 00:47:28 +0100

    message

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,23 @@
   import '$lib/styles/theme.css';
   import { onMount } from 'svelte';
 
+  let music;
+  let started = false;
+
   onMount(async () => {
     await import('bootstrap/dist/js/bootstrap.bundle.min.js');
+
+    // music trigger
+    const startMusic = () => {
+      if (music) {
+        music.volume = 0.4;
+        music.play().catch(() => {});
+      }
+    };
+
+    // interaction events that browsers accept
+    window.addEventListener('scroll', startMusic, { once: true });
+    window.addEventListener('click', startMusic, { once: true });
   });
 
   import Navbar from '$lib/components/Navbar.svelte';
@@ -16,6 +31,9 @@
 
 <main>
   <CursorRadar />
+  <audio bind:this={music} loop>
+    <source src="/schubert.mp3" type="audio/mpeg" />
+  </audio>
   <slot />
 </main>
 
