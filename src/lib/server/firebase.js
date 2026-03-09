fa9b8b694cac7bfb6d0d123ac0f4571c92bc3fe2
Filipe Bertolucci <filipe@bertolucci.it>
2026-03-09 21:54:24 +0100

    message

--- src/lib/server/firebase.js
+++ src/lib/server/firebase.js
@@ -1,16 +1,23 @@
 import admin from 'firebase-admin';
 import { env } from '$env/dynamic/private';
 
-if (!admin.apps.length) {
+let db;
+
+if (
+  !admin.apps.length &&
+  env.FIREBASE_PROJECT_ID &&
+  env.FIREBASE_CLIENT_EMAIL &&
+  env.FIREBASE_PRIVATE_KEY
+) {
   admin.initializeApp({
     credential: admin.credential.cert({
       projectId: env.FIREBASE_PROJECT_ID,
       clientEmail: env.FIREBASE_CLIENT_EMAIL,
-      privateKey: env.FIREBASE_PRIVATE_KEY
-        ? env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
-        : undefined,
+      privateKey: env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   });
+
+  db = admin.firestore();
 }
 
-export const db = admin.firestore();
+export { db };
