972169824c0ea4047eca9986ce8e68bfea9f9f35
Filipe Bertolucci <filipe@bertolucci.it>
2026-03-09 20:22:30 +0100

    message

--- src/lib/server/firebase.js
+++ src/lib/server/firebase.js
@@ -1,16 +1,12 @@
 import admin from 'firebase-admin';
-import {
-  FIREBASE_PROJECT_ID,
-  FIREBASE_CLIENT_EMAIL,
-  FIREBASE_PRIVATE_KEY,
-} from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: FIREBASE_PROJECT_ID,
-      clientEmail: FIREBASE_CLIENT_EMAIL,
-      privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      projectId: env.FIREBASE_PROJECT_ID,
+      clientEmail: env.FIREBASE_CLIENT_EMAIL,
+      privateKey: env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   });
 }
