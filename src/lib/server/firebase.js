01f0ff305fa8004c0d557df92ac733b73c4a1b7c
Filipe Bertolucci <filipe@bertolucci.it>
2026-03-09 21:47:14 +0100

    message

--- src/lib/server/firebase.js
+++ src/lib/server/firebase.js
@@ -6,7 +6,9 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: env.FIREBASE_PROJECT_ID,
       clientEmail: env.FIREBASE_CLIENT_EMAIL,
-      privateKey: env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      privateKey: env.FIREBASE_PRIVATE_KEY
+        ? env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
+        : undefined,
     }),
   });
 }
